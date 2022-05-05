5f9a8c019317a4950fdf9daa3a5b0a724352cf54
Hansel López <halm29799@gmail.com>
2022-05-05 12:01:10 -0600

    Create React base project with webpack and babel

---
+++ src/components/calculator/Calculator.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './calculator.scss'
+
+const Calculator = () => (
+  <>
+    <h1 className="title">Calculator</h1>
+  </>
+)
+
+export default Calculator
---
+++ src/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Calculator from './components/calculator/Calculator'
+
+const App = () => (
+  <Router>
+    <Routes>
+      <Route
+        path="/"
+        element={(
+          <Calculator />
+        )}
+      />
+    </Routes>
+  </Router>
+)
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />)
